4ca60e7b60ccb97883d2618fd473ce9cdd1501a4
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-12 02:52:39 +0700

    refactor: standardize code formatting in useScan.js for consistency

--- src/hooks/useScan.js
+++ src/hooks/useScan.js
@@ -1,5 +1,5 @@
-import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
-import axiosClient from "@/api/axios.js";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axiosClient from '@/api/axios.js';
 
 export const useScanImage = () => {
     const queryClient = useQueryClient();
@@ -14,24 +14,24 @@ export const useScanImage = () => {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-            await queryClient.invalidateQueries({queryKey: ['scans']});
+            await queryClient.invalidateQueries({ queryKey: ['scans'] });
             return response.data.data;
         },
         onError: (error) => {
-            console.error("Gagal melakukan scan:", error);
-        }
+            console.error('Gagal melakukan scan:', error);
+        },
     });
 };
 
 const fetchScans = async () => {
     const response = await axiosClient.get('/scans/history');
     return response.data.data;
-}
+};
 
 export const useScans = () => {
     return useQuery({
-        queryKey: ["scans"],
+        queryKey: ['scans'],
         queryFn: fetchScans,
         staleTime: 1000 * 60 * 5,
-    })
-}
+    });
+};
