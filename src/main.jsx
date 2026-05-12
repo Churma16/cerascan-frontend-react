00852c14ab192200df7a001cb5922027d7d137ee
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-13 00:31:56 +0700

    style: clean up QueryClient configuration for improved readability

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,11 @@ import App from './App.jsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Inisialisasi QueryClient
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            refetchOnWindowFocus: false, // Mencegah fetch ulang otomatis saat pindah tab browser
-            retry: 1, // Jika gagal, coba lagi 1 kali
+            refetchOnWindowFocus: false,
+            retry: 1,
         },
     },
 });
