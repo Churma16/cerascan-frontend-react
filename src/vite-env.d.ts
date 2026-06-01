98c69497eea051e6a52cc41989a607a9acea8520
Fathan <74785057+Churma16@users.noreply.github.com>
2026-06-01 15:52:43 +0700

    feat(vite-env): add Midtrans and WebSocket environment variables for payment and real-time features

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,16 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_PROD: string;
-  readonly VITE_BACKEND_URL_PROD: string;
-  readonly VITE_BACKEND_URL_DEV: string;
-  readonly VITE_BACKEND_STORAGE_URL_DEV:string;
-  readonly VITE_BACKEND_STORAGE_URL_PROD:string;
+    readonly VITE_PROD: string;
+    readonly VITE_BACKEND_URL_PROD: string;
+    readonly VITE_BACKEND_URL_DEV: string;
+    readonly VITE_BACKEND_STORAGE_URL_DEV: string;
+    readonly VITE_BACKEND_STORAGE_URL_PROD: string;
+    readonly VITE_MIDTRANS_CLIENT_KEY: string;
+    readonly VITE_MIDTRANS_SNAP_URL: string;
+    readonly VITE_WEB_SOCKET_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+    readonly env: ImportMetaEnv;
 }
