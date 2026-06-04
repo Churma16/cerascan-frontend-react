fcd86fc9c16bfeecf96e8292c3367bd4c686ecc6
Fathan <74785057+Churma16@users.noreply.github.com>
2026-06-04 15:25:55 +0700

    style(AdminPageHeader): add div wrapper

--- src/components/AdminPageHeader.jsx
+++ src/components/AdminPageHeader.jsx
@@ -1,8 +1,10 @@
 export default function AdminPageHeader({ title, desc }) {
     return (
-        <div>
-            <h2 className="text-2xl font-black tracking-tight text-[#042B1F]">{title}</h2>
-            <p className="text-sm font-medium text-gray-500 mt-1">{desc}</p>
+        <div className="flex justify-between items-end mb-8">
+            <div>
+                <h2 className="text-2xl font-black tracking-tight text-[#042B1F]">{title}</h2>
+                <p className="text-sm font-medium text-gray-500 mt-1">{desc}</p>
+            </div>
         </div>
     );
 }
