7cb29d60c090db9e00b63e67f21daacf53103a93
Fathan <74785057+Churma16@users.noreply.github.com>
2026-06-04 15:20:25 +0700

    style(PageWrapper):add animation and fix the layouting of the wrapper

--- src/layouts/PageWrapper.jsx
+++ src/layouts/PageWrapper.jsx
@@ -1,7 +1,9 @@
 export default function PageWrapper({ children }) {
     return (
-        <div className="flex-1 overflow-y-auto p-8 relative z-10">
-            <div className="max-w-6xl mx-auto">{children}</div>
+        <div className="flex-1 overflow-y-auto px-8 py-8 pb-16 relative z-10">
+            <div className="max-w-6xl mx-auto space-y-6 animate-in fade-in slide-in-from-bottom-4 duration-500">
+                {children}
+            </div>
         </div>
     );
 }
